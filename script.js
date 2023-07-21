0940f6dde6d8bacbdbb1d48296127a53f10ee432
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2023-07-21 10:58:48 -0500

    Removed PHP files

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-window.onload = placeGrayBoars;
+window.onload = () => {
+    placeNav();
+    placeGrayBoars();
+};
 window.onscroll = fixNavBar;
+
+async function placeNav() {
+    const body = document.getElementsByTagName('body')[0];
+    const navHTML = await getFileText('nav.html');
+
+    body.innerHTML = navHTML + body.innerHTML;
+}
 
 function placeGrayBoars() {
     const body = document.getElementsByTagName('body')[0];
@@ -7,22 +17,35 @@ function placeGrayBoars() {
     const boarSpacing = window.innerHeight;
     const numBoars = Math.ceil(bodyHeight / boarSpacing);
 
+    const grayBoarDiv = document.createElement('div');
+
     for (let i=0; i<numBoars; i++) {
         const grayBoarElement = document.createElement('img');
         grayBoarElement.className = 'nav-img';
         grayBoarElement.src = 'images/BoarBotMascotGray.png';
         grayBoarElement.style.top = (i * boarSpacing - 150) + 'px';
-        body.appendChild(grayBoarElement);
+        grayBoarDiv.appendChild(grayBoarElement);
     }
+
+    body.appendChild(grayBoarDiv);
 }
 
 function fixNavBar() {
-    const body = document.getElementsByTagName('body')[0];
-    const navBar = document.getElementById('nav');
+    const navBar = document.getElementsByTagName('nav')[0];
+    const boarLogo = document.getElementById('nav-left').children[0];
 
     if (window.scrollY > 100) {
-        navBar.style.backgroundColor = '#151518';
+        navBar.style.backgroundColor = '#151518bb';
+        navBar.style.height = '75px';
     } else {
         navBar.style.backgroundColor = '#15151800';
+        navBar.style.height = '100px';
     }
+}
+
+async function getFileText(path) {
+    const result = await fetch(path);
+    const resultText = await result.text();
+
+    return resultText;
 }
