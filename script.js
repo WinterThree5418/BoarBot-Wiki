17ef08eec24c76134c1154808d10683c2c4f0b2b
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2023-07-22 14:52:01 -0500

    Better mobile navigation, New images

--- script.js
+++ script.js
@@ -1,20 +1,72 @@
-window.onload = () => {
-    placeNav();
+window.onload = async () => {
     placeGrayBoars();
     fixBottomMargin();
     history.pushState('', document.title, window.location.pathname + window.location.search);
+
+    await placeNav();
+    fixNavBar();
+
+    window.onscroll = fixNavBar;
+    window.onresize = () => {
+        fixBottomMargin();
+        fixNavMenu();
+    };
 };
 window.onhashchange = () => {
     history.pushState('', document.title, window.location.pathname + window.location.search);
-}
-window.onscroll = fixNavBar;
-window.onresize = fixBottomMargin;
+};
+
+let menuOpen = false;
+let inDesktop = window.innerWidth >= 1000;
 
 async function placeNav() {
     const body = document.getElementsByTagName('body')[0];
     const navHTML = await getFileText('/nav.html');
 
     body.innerHTML = navHTML + body.innerHTML;
+
+    const menuBtn = document.getElementById('hamburger-menu').children[0];
+    menuBtn.addEventListener('click', openMenu);
+}
+
+function openMenu(ev, menuState) {
+    const navBar = document.getElementsByTagName('nav')[0];
+    const menuBtn = document.getElementById('hamburger-menu').children[0];
+    const menu = document.getElementById('nav-items');
+
+    if (menuState !== undefined) {
+        menuOpen = !menuState;
+    }
+
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+
+        navBar.style.backgroundColor = '#151518bb';
+        navBar.style.height = '75px';
+        menu.style.display = 'flex';
+
+        if (window.scrollY <= 100) {
+            setTimeout(() => {
+                menu.style.opacity = '100%';
+            }, 500);
+        } else {
+            menu.style.opacity = '100%';
+        }
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+
+        menu.style.opacity = '0';
+
+        setTimeout(() => {
+            menu.style.display = 'none';
+            if (window.scrollY <= 100) {
+                navBar.style.backgroundColor = '#15151800';
+                navBar.style.height = '100px';
+            }
+        }, 333);
+    }
 }
 
 function placeGrayBoars() {
@@ -44,7 +96,7 @@ function fixNavBar() {
     if (window.scrollY > 100) {
         navBar.style.backgroundColor = '#151518bb';
         navBar.style.height = '75px';
-    } else {
+    } else if (!menuOpen || inDesktop) {
         navBar.style.backgroundColor = '#15151800';
         navBar.style.height = '100px';
     }
@@ -54,10 +106,34 @@ function fixBottomMargin() {
     if (window.location.pathname !== '/') return;
 
     const home = document.getElementById('home');
-    if (window.innerWidth < 900) {
+    if (window.innerWidth < 1000) {
         home.style.marginBottom = Math.min(175, 50 + Math.max(0, window.innerWidth - 500) / 2) + 'px';
     } else {
         home.style.marginBottom = '175px';
+    }
+}
+
+function fixNavMenu() {
+    const navBar = document.getElementsByTagName('nav')[0];
+    const menu = document.getElementById('nav-items');
+    
+    if (window.scrollY <= 100 && (!menuOpen || inDesktop)) {
+        navBar.style.backgroundColor = '#15151800';
+        navBar.style.height = '100px';
+    } else {
+        navBar.style.backgroundColor = '#151518bb';
+        navBar.style.height = '75px';
+    }
+
+    if (window.innerWidth >= 1000 && !inDesktop) {
+        inDesktop = true;
+        menu.style.display = 'flex';
+        menu.style.opacity = '100%';
+    } else if (window.innerWidth < 1000 && inDesktop) {
+        inDesktop = false;
+        menu.style.display = 'none';
+        menu.style.opacity = 0;
+        setTimeout(() => openMenu(undefined, menuOpen), 10);
     }
 }
 
