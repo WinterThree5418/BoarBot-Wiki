035f031afc99ffafe79d4ff3f9ea368658213069
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2025-01-14 20:35:24 -0600

    Use docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/extra/Skyblock/BoarBotWiki/node_modules/infima/dist/css/default/default.css"),
+  require("/extra/Skyblock/BoarBotWiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/extra/Skyblock/BoarBotWiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/extra/Skyblock/BoarBotWiki/src/css/custom.css"),
+];
