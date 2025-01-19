1ae3947f5442de9153550984ab1b64a18e42026c
WeslayCodes <60010287+WeslayCodes@users.noreply.github.com>
2025-01-19 13:45:51 -0600

    Fix

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/extra/Skyblock/BoarBotWiki/node_modules/infima/dist/css/default/default.css"),
-  require("/extra/Skyblock/BoarBotWiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/extra/Skyblock/BoarBotWiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/extra/Skyblock/BoarBotWiki/src/css/custom.css"),
-];
